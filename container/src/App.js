3cc7a0e70c92a15ad0613dff952a33434a9ba1e5
Yasuaki Toyoda <66811920+YodaRoshi@users.noreply.github.com>
2025-01-21 21:40:46 -0500

    add shared properties

---
+++ container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+import MarketingApp from './components/MarketingApp';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
+export default App;
